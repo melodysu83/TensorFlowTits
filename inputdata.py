932bfb4b94ff1db7bd8edfe73ba64e9c1d593bee
Melody <melodysu83@gmail.com>
2016-11-27 06:26:08 -0800

    Update inputdata.py

--- inputdata.py
+++ inputdata.py
@@ -43,6 +43,17 @@ class Dataset:
 				image_data = image_data + [intensity_value]
 		return image_data
 
+	def load_square_image_from_disk(self,class_label,filename, reshape_size):
+		image_data = []
+		path_of_image = self.paths[class_label] + str(filename) + self.image_type
+		image = cv2.imread(path_of_image)
+		image = cv2.resize(image, (reshape_size, reshape_size)) 
+
+		for row in range(reshape_size):
+			for col in range(reshape_size):
+				intensity_value = image[row,col,0]
+				image_data = image_data + [intensity_value]
+		return image_data
 
 	def images_to_data_batch(self,batch_number,batch_size):
 		first_loop = True
@@ -81,3 +92,40 @@ class Dataset:
 					images_label = np.concatenate((images_label,image_label))
 		return images_data,images_label
 
+
+	def images_to_square_data_batch(self,batch_number,batch_size,reshape_size):
+		first_loop = True
+		for _ in range(batch_size):
+			filename = str(batch_number*batch_size + 1)
+			for class_label in range(self.number_of_classes):
+				image_data = np.array([self.load_square_image_from_disk(class_label,filename, reshape_size)])
+				image_label = np.array([self.create_one_hot_label(class_label)])
+				#image_label = np.array([self.create_numeric_label(class_label)])
+				
+				if first_loop:
+					first_loop = False
+					images_data = image_data
+					images_label = image_label
+				else:
+					images_data = np.concatenate((images_data,image_data))
+					images_label = np.concatenate((images_label,image_label))
+		return images_data,images_label
+
+
+	def get_square_test_set(self,reshape_size):
+		first_loop = True
+		for index in range(self.test_set_size):
+			filename = self.test_set[index]
+			for class_label in range(self.number_of_classes):
+				image_data = np.array([self.load_square_image_from_disk(class_label,filename, reshape_size)])
+				image_label = np.array([self.create_one_hot_label(class_label)])
+				#image_label = np.array([self.create_numeric_label(class_label)])
+
+				if first_loop:
+					first_loop = False
+					images_data = image_data
+					images_label = image_label
+				else:
+					images_data = np.concatenate((images_data,image_data))
+					images_label = np.concatenate((images_label,image_label))
+		return images_data,images_label
