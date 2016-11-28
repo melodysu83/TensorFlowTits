83c404297ee6f6c647780f30c0a584f3701c0336
Melody <melodysu83@gmail.com>
2016-11-27 19:28:10 -0800

    Update myproject.py

--- myproject.py
+++ myproject.py
@@ -1,24 +1,23 @@
 from controlmenu import Menu
 
 def main():	
-	# choose machine learning model
-	# (0: MLP, 1: RNN, 2: CNN)
-	model =  1      
-	training = True
-	testing = True	
+	# choose machine learning model	
+	model =  2  # (0: MLP,      1: RNN,     2: CNN)
+	mode =   2  # (0: training, 1: testing, 2: real world implementation)  
 
 	# setup
-	menu = Menu(training,testing)	
-
+	menu = Menu(mode)	
+	
 	# start running
 	if model == menu.MLP: 			
 		menu.MLP_Process(15,100)     # epochs, batchsize
 
 	elif model == menu.RNN: 
-		menu.RNN_Process(15,100,28)   # epochs, batchsize, chunk
+		menu.RNN_Process(15,128,28)   # epochs, batchsize, chunk
 	
 	elif model == menu.CNN:
 		menu.CNN_Process(15,128,0.8) # epochs, batchsize, keep_rate
+	
 	
 	
 if __name__ == "__main__":
